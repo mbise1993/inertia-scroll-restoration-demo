7490ba3908258cda5b2275c8f94bb07399702ba4
Matt Bise <mbise1993@gmail.com>
2024-03-02 04:14:06 -0600

    Cause the error

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,14 +1,26 @@
-import { createInertiaApp } from '@inertiajs/react'
-import { createRoot } from 'react-dom/client'
+import { createInertiaApp } from "@inertiajs/react";
+import { createRoot } from "react-dom/client";
 
 createInertiaApp({
-  title: (title) => `${title} - React Playground`,
-  resolve: (name) => {
-    const pages = import.meta.glob('./Pages/**/*.tsx', { eager: true })
-    return pages[`./Pages/${name}.tsx`]
-  },
-  setup({ el, App, props }) {
-    // hydrateRoot(el, <App {...props} />)
-    createRoot(el).render(<App {...props} />)
-  },
-})
+    title: (title) => `${title} - React Playground`,
+    resolve: (name) => {
+        const pages: Record<string, () => Promise<any>> = import.meta.glob(
+            "./Pages/**/*.tsx"
+        );
+        const page = pages[`./Pages/${name}.tsx`];
+        if (!page) {
+            throw new Error(`Unable to resolve page for route "${name}"`);
+        }
+
+        const pagePromise = page();
+        Promise.resolve(pagePromise).then((pageModule) => {
+            pageModule.default.layout = pageModule.default.layout ?? []; // set some default AppLayout here
+        });
+
+        return pagePromise;
+    },
+    setup({ el, App, props }) {
+        // hydrateRoot(el, <App {...props} />)
+        createRoot(el).render(<App {...props} />);
+    },
+});
